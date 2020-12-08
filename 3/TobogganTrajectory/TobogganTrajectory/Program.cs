4916b7dadde502b34fd2cdf400a979f2b6d3a31a
Mathijs de Bruin <m.debruin@evident.nl>
2020-12-08 09:52:10 +0100

    3-2

--- 3/TobogganTrajectory/TobogganTrajectory/Program.cs
+++ 3/TobogganTrajectory/TobogganTrajectory/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace TobogganTrajectory
@@ -13,6 +14,28 @@ namespace TobogganTrajectory
             slope.MovePersonToBottomWhileCountingTrees(3, 1);
 
             Console.WriteLine("Amount of trees passed: {0}", slope.Person.TreesCounted);
+
+            var traversals = new List<(int, int)>
+            {
+                (1, 1),
+                (3, 1),
+                (5, 1),
+                (7, 1),
+                (1, 2),
+            };
+
+            var sums = new List<int>();
+
+            traversals.ForEach(x =>
+            {
+                var slope = new Slope(lines.ToList());
+                slope.MovePersonToBottomWhileCountingTrees(x.Item1, x.Item2);
+                sums.Add(slope.Person.TreesCounted);
+            });
+
+            var multiplied = sums.Aggregate((a, x) => a * x);
+
+            Console.WriteLine("Amount of trees passed multiplied: {0}", multiplied);
             Console.ReadLine();
         }
     }
